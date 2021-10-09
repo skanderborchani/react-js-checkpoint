0eeb21ea6b8ca7bc94b8d89f7a03f4f168c501b7
skanderborchani <85353222+skanderborchani@users.noreply.github.com>
2021-10-09 12:07:45 +0100

    react js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p> welcome to our form all you need to do is type in your email and your password it's simple then click on submit </p>
+     <form>
+  <div class="form-group">
+    <label for='exampleInputEmail1'>Email address</label>
+    <input 
+    type='email'
+     class='formControl'
+      id='exampleInputEmail1'
+       ariaDescribedby='emailHelp'
+        placeholder='Enter email'
+        />
+    <small id='emailHelp' class='FormTextTextMuted'>We'll never share your email with anyone else.</small>
+  </div>
+  <div className='formGroup'>
+    <label for='exampleInputPassword1'>Password</label>
+    <input
+     type='password'
+      class='formControl'
+       id='exampleInputPassword1'
+        placeholder='Password'
+        />
+  </div>
+  <div className='formGroup formCheck'>
+    <input
+     type='checkbox'
+      class='form-check-input' 
+      id='exampleCheck1'
+      />
+    <label class='formCheckLabel' for='exampleCheck1'>Check me out</label>
+  </div>
+  <button type="submit" class='BtnBtnPrimary'>Submit</button>
+</form>
+<p className='thx'>thank you for using our website</p>
     </div>
   );
 }
